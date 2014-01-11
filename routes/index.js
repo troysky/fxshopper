f1495e91dfea4ef4dd6a11ec7c757c8076b4ab39
jnd <skybluezone@gmail.com>
2014-01-12 10:45:31 +1100

    stage 1 implementation

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
-
-/*
- * GET home page.
- */
+var $ = require('jquery');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	$.ajax({
+        url: 'http://www.google.com/',
+        dataType: "jsonp",
+        jsonpCallback: "_test",
+        cache: false,
+        timeout: 5000,
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert('error ' + textStatus + " " + errorThrown);
+        }
+    });
+
+	res.render('index', { title: 'Express' });
 };
+
+function _test(){
+	console.log("testing");
+}
